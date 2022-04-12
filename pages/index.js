f0d1d32feadd12015feb76c6f290e7fd2901dfba
HeeYoung-Youn <yhy1@naver.com>
2022-04-12 14:41:29 +0900

    Add: Routing detail page
    Routing detail page, masking query

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,32 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) =>
+    router.push(
+      { pathname: `/movies/${id}`, query: { title } },
+      `/movies/${id}`
+    );
   return (
     <div className="container">
       <Seo title="Home" />
-      {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+      {results?.map(({ id, title, original_title, poster_path }) => (
+        <div onClick={() => onClick(id, title)} className="movie" key={id}>
+          <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} />
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${id}`,
+                query: { title },
+              }}
+              as={`/movies/${id}`}
+            >
+              <a>{original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
